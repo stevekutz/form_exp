90e3e3d5cdd602d8535d7c5ae1be3df4d77b5f82
stevekutz <stkutz@gmail.com>
2020-03-06 15:42:34 -0600

    added Check component & State Inspector

---
+++ app/src/comp/MultiCheck.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {useState} from 'reinspect';
+import {Input, List, Segment} from 'semantic-ui-react';
+
+const style = {
+    listContainer: {
+        listStyle: 'none',
+        paddingLeft: 0
+    },
+    itemStyle: {
+        cursor: 'pointer',
+        padding: 5
+
+    }
+
+
+}
+
+// const options = [{label: 'First Item'}, {label: 'Second Item'}];
+
+function MultiCheck({options, onChange}) {
+    // const [data, setData] = React.useState(options, 'data state');
+    const [data, setData] = useState(options, "Options State");
+
+
+    const toggle = item => {
+        data.map((_, key) => {
+            console.log('_ is ', _);
+            if(data[key].label === item.label) data[key].checked = !item.checked;
+        });
+        setData([...data]);
+        onChange(data);
+    }
+
+
+
+    return (
+        <Segment style = {{border: "1px solid red"}}>
+            <List style = {style.listContainer}>
+                {data.map(item => {
+                    return (
+                        <List.Item key = {item.label} style = {style.itemStyle} onClick = {()=> toggle(item)}>
+                            <Input  readOnly type = 'checkbox' checked = {item.checked || false }/>
+                            {item.label}
+                        </List.Item>         
+                    );                
+                })}       
+            </List>
+        </Segment>
+    )
+
+};
+
+export default MultiCheck
