abd9ac3a22c99794bde11ca9c971c57ff4969c9c
stevekutz <stkutz@gmail.com>
2020-03-07 20:29:11 -0600

    added basic form

--- app/src/App.js
+++ app/src/App.js
@@ -6,7 +6,37 @@ function App() {
   return (
     <div className="App">
 
-    
+        <div style = {{width: '30%', margin: '10px auto'}}> 
+                    
+            <div> 
+                
+                <label> enter todo description: </label>
+                <input
+                    style = {{width: '100%', outlineStyle: 'none'}}
+                    placeholder = 'the placeholder text'
+                    value = {task}
+                    onChange = {handleChange}          
+                />
+            
+                <form  onSubmit = {handleSubmit}>
+                    <div> 
+                        <div>
+                            <button type = 'submit' > Add Todo </button> 
+                            <button onClick = {clearTodos}> Clear All Todos</button>                                   
+                        </div>
+                    </div>
+                
+                <input
+                    style = {{width: '100%', outlineStyle: 'none'}}
+                    placeholder = 'the placeholder text'
+                    value = {task}
+                    onChange = {handleChange}          
+                />    
+
+                </form>
+            </div>
+
+        </div>
 
 
     </div>
